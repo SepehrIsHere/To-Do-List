771f1f031fc993897b770ea3ec88e2ce9c35daa2
Sepehr <sepehristaken@gmail.com>
2025-03-01 06:36:03 -0800

    update repository

--- src/main/java/org/practice/simpletodo/repository/TaskRepository.java
+++ src/main/java/org/practice/simpletodo/repository/TaskRepository.java
@@ -2,6 +2,7 @@ package org.practice.simpletodo.repository;
 
 import org.practice.simpletodo.entities.Task;
 import org.practice.simpletodo.entities.TaskStatus;
+import org.practice.simpletodo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,6 +19,18 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     @Query("SELECT t FROM Task t WHERE t.id = :id")
     Optional<List<Task>> findByUser(@Param("id") Long id);
 
+    @Query("SELECT t FROM Task t WHERE t.user.id = : id AND t.priority = :LOW")
+    Optional<List<Task>> findLowPriorityTask(@Param("id") Long id);
+
+    @Query("SELECT t FROM Task t WHERE t.user.id = : id AND t.priority = :MEDIUM")
+    Optional<List<Task>> findMediumPriorityTask(@Param("id") Long id);
+
+    @Query("SELECT t FROM Task t WHERE t.user.id = : id AND t.priority = :HIGH")
+    Optional<List<Task>> findHighPriorityTask(@Param("id") Long id);
+
+    @Query("SELECT t FROm Task t WHERE t.user.id = : id AND t.priority = :URGENT")
+    Optional<List<Task>> findUrGentPriorityTask(@Param("id") Long id);
+
     @Query("SELECT t FROM Task t WHERE t.user.id = :id AND  t.status = :COMPLETED")
     Optional<List<Task>> findCompleteTasks(@Param("id") Long id);
 
@@ -26,5 +39,9 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
     @Query("SELECT t FROM Task t WHERE t.user.id = :id AND  t.status = :FAILED")
     Optional<List<Task>> findFailedTasks(@Param("id") Long id);
+
+    Long user(User user);
+
+    Long user(User user);
 }
     
